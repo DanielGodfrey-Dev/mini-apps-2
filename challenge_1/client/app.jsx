4cc708f6a1034f9e0e443fae091054494b852c0a
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-04-23 10:29:44 -0700

    Implement text search bar to query json server and return paginated data
    Confirmed working

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -7,26 +7,49 @@ class App extends React.Component {
         super(props);
         this.state = {
             data: [],
-            query: 'Macedonia',
+            query: '',
+            searchedTerm: '',
             page: 1
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    componentDidMount() {
+
+    handleChange(event) {
+        this.setState({query: event.target.value});
+        console.log(this.state.query);
+      }
+    
+    handleSubmit(event) {
         axios.get(`api/events?q=${this.state.query}&_page=${this.state.page}`)
         .then((results) => {
-            this.setState({ data: results.data })
+            let query = this.state.query;
+            this.setState({ data: results.data, searchedTerm: query })
             console.log('success: ', this.state.data);
         })
         .catch((err) => {
             console.log(err);
         })
+        event.preventDefault();
     }
 
     render() {
 
         return (
-            <div>Welcome to History. Be Forewarned.</div>
+            <div>
+                <div>
+                    <h1>Welcome to History. Be Forewarned.</h1>
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Query Historical Records:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                {this.state.searchedTerm ? <div>You searched for <span style={{ color: 'red' }}>{this.state.searchedTerm}</span>...</div> : null}
+            </div>
 
         )
     }
