bad13697a2fd05e3396da512f8211dc316f36ca4
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-04-24 15:42:57 -0700

    Setup dev environment, configure webpack, install dependencies, render App to DOM
    up and running

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,24 @@
 
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    render() {
+
+        return (
+            <div>...LET'S BOWL</div>
+
+        )
+
+    }
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+const port = 3002;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`bowling in 2099AD on a port even more advanced...port ${port}`));
 
