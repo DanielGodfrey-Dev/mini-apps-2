288975b0d4cd54df73fab857a15e02a6d572e8d0
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-04-23 22:00:32 -0700

    Configured client and server to fetch coindesk bpi data by data range

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from 'chart.js';
+import axios from 'axios';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            bpi: null,
+            disclaimer: null,
+            time: null
         }
     }
 
     componentDidMount() {
-        console.log('cyber currency data preparing to initialize...')
-    }
+        axios.post('/api/BTC', {
+            start: '2013-09-01',
+            end: '2013-09-10'
+        })
+        .then((results) => {
+            this.setState({ 
+                bpi: results.data.bpi,
+                disclaimer: results.data.disclaimer,
+                time: results.data.time
+            });
+            console.log(this.state);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    };
 
     render() {
         return (
